101056439e5ef4b055f4c93727e23cc9a0b5c8c8
Muhammad Usman <m.usman12529@gmail.com>
2023-09-03 22:08:00 -0230

    commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const server = require('http').createServer(app);
+app.use(express.static(path.join(__dirname, 'public')));
+server.listen(process.env.PORT || 5000);
+
+const users = {};
+
+const io= require('socket.io')(server);
+io.on('connection', socket => {
+
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    }); 
+    console.log('new user');
+    
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]});
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
+        delete users[socket.id];
+    });
+})});
